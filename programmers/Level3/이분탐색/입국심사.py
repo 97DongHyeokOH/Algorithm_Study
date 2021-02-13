bdfe3f059828ff645e33b5ad153e8c126527913c
yserbane <donggar97@naver.com>
2021-02-13 19:32:53 +0900

    프로그래머스 레벨3 입국심사

---
+++ programmers/Level3/이분탐색/입국심사.py
@@ -0,0 +1,23 @@
+def solution(n, times):
+    left = 1
+    right = n * min(times)
+    result = 0
+
+    while(left <= right):
+        mid = (left + right) // 2
+        temp = 0
+
+        for time in times:
+            temp += mid // time
+            if(temp >= n):
+                result = mid
+                right = mid-1
+                break
+
+        if(temp < n):
+            left = mid+1
+
+    return result
+
+
+print(solution(6, [7, 10]))
