fd656b73fbbac947e9260b5673745a0b38d45ffe
yserbane <donggar97@naver.com>
2021-07-01 20:39:35 +0900

    BAEKJOON_1095

--- BAEKJOON/Practice/기타/1095.py
+++ BAEKJOON/Practice/기타/1095.py
@@ -10,7 +10,7 @@
 
 import sys
 
-# 팩토리얼을 소인수 분해
+# n팩토리얼을 소인수 분해
 
 
 def prime_fact(n):
@@ -29,14 +29,14 @@ def prime_fact(n):
 
     return prime_fact_arr
 
-# 그냥 자연수를 소인수 분해
+# 그냥 자연수를 소인수 분해한 것을 list에 [소수, 지수]로 저장해서 넣는다.
 
 
 def prime_fact2(n):
-    prime_fact_arr = [0]*(m+1)
+    prime_fact_arr = []
 
     for i in primes:
-        if(i > n):
+        if(n == 1):
             return prime_fact_arr
 
         k = 0
@@ -45,43 +45,70 @@ def prime_fact2(n):
             n //= i
             k += 1
 
-        prime_fact_arr[i] = k
+        prime_fact_arr.append([i, k])
 
     return prime_fact_arr
 
 
+# 입력을 받음
 s, f, m = map(int, sys.stdin.readline().split())
 
+# idx가 소수인지 아닌지를 저장하는 list
 num = [False, False] + [True]*(m-1)
+# 소수를 저장해주는 list
 primes = []
+# 이 문제의 답
 ans = 0
 
+# 에라토스 테네스의 체로 m이하의 소수를 걸러내서 primes에 저장해 준다.
+# m이하의 소수만 구하는 이유는 m보다 큰 소수는 결과 값에 영향을 주지 않기 때문이다.
+# 그렇게 때문에 소인수 분해를 할 때도 소수가 m이하인 소수만으로 소인수 분해를 해준다.
 for i in range(2, m+1):
     if(num[i]):
         primes.append(i)
         for j in range(i+i, m+1, i):
             num[j] = False
 
+# 소수들을 set자료형으로 임의로 하나 만들어 줌
+temp_primes = set(primes)
+
+# (s+f)! 소인수 분해
 prime_fact_denominator = prime_fact(s+f)
+# s! 소인수 분해
 prime_fact_numerator_1 = prime_fact(s)
+# f! 소인수 분해
 prime_fact_numerator_2 = prime_fact(f)
 
+# 조합을 소인수 분해한 결과를 저장해주는 list -> result_pimre_fact[i] = k 는 i인 소수의 지수가 k라는 의미
 result_prime_fact = [0]*(m+1)
 
-for i in range(2, m+1):
+for i in primes:
     result_prime_fact[i] = prime_fact_denominator[i] - \
         prime_fact_numerator_1[i] - prime_fact_numerator_2[i]
 
+# m부터 시작해서 i로 나눠떨어지는지 확인해 최대값을 도출해 냄
 for i in range(m, 0, -1):
+    # i가 소수라면 조합을 소인수 분해한 결과에 소인수로 i가 있는지 확인
+    # 이때 시간을 조금이라도 더 줄이기 위해 set자료형을 사용했음...
+    if(i in temp_primes):
+        if(result_prime_fact[i]):
+            print(i)
+            break
+        else:
+            continue
+
+    # i로 나눠지는지 판단
+    able = True
+    # i를 소인수 분해 해줌
     prime_f = prime_fact2(i)
 
-    for idx in primes:
-        if(result_prime_fact[idx] < prime_f[idx]):
+    # 나눠 떨어지는지 확인
+    for idx, k in prime_f:
+        if(result_prime_fact[idx] < k):
+            able = False
             break
 
-        if(idx == primes[len(primes)-1]):
-            ans = i
-
-    if(ans):
-        print(ans)
+    # 나눠떨어진다면 출력하고 for문 종료
+    if(able):
+        print(i)
         break
