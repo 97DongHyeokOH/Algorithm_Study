8a0f056a825131024ddad99c8677a7d0e9089325
yserbane <donggar97@naver.com>
2021-07-06 13:21:10 +0900

    BAEKJOON_2665

--- BAEKJOON/Practice/다익스트라/2665.py
+++ BAEKJOON/Practice/다익스트라/2665.py
@@ -1,91 +1,63 @@
+# 문제: n×n 바둑판 모양으로 총 n2개의 방이 있다. 일부분은 검은 방이고 나머지는 모두 흰 방이다. 검은 방은 사면이 벽으로 싸여 있어 들어갈 수 없다. 서로 붙어 있는 두 개의 흰 방 사이에는 문이 있어서 지나다닐 수 있다. 윗줄 맨 왼쪽 방은 시작방으로서 항상 흰 방이고, 아랫줄 맨 오른쪽 방은 끝방으로서 역시 흰 방이다.
+# 시작방에서 출발하여 길을 찾아서 끝방으로 가는 것이 목적인데, 아래 그림의 경우에는 시작방에서 끝 방으로 갈 수가 없다. 부득이 검은 방 몇 개를 흰 방으로 바꾸어야 하는데 되도록 적은 수의 방의 색을 바꾸고 싶다.
+# 아래 그림은 n=8인 경우의 한 예이다.
+# 위 그림에서는 두 개의 검은 방(예를 들어 (4,4)의 방과 (7,8)의 방)을 흰 방으로 바꾸면, 시작방에서 끝방으로 갈 수 있지만, 어느 검은 방 하나만을 흰 방으로 바꾸어서는 불가능하다. 검은 방에서 흰 방으로 바꾸어야 할 최소의 수를 구하는 프로그램을 작성하시오.
+# 단, 검은 방을 하나도 흰방으로 바꾸지 않아도 되는 경우는 0이 답이다.
+
+# 입력: 첫 줄에는 한 줄에 들어가는 방의 수 n(1≤n≤50)이 주어지고, 다음 n개의 줄의 각 줄마다 0과 1이 이루어진 길이가 n인 수열이 주어진다. 0은 검은 방, 1은 흰 방을 나타낸다.
+
+# 출력: 첫 줄에 흰 방으로 바꾸어야 할 최소의 검은 방의 수를 출력한다.
+
 import sys
 import heapq
 
+# queue에 집어넣도 되는지 판단
+
 
 def possible(y, x):
-    if(0 <= y < n and 0 <= x < n and miro[y][x] == '1' and not visit[y][x]):
+    if(0 <= y < n and 0 <= x < n and not visit[y][x][0]):
         return True
     return False
 
 
-def bfs(i, j):
-    queue = [(i, j)]
-    temp = [(i, j)]
-
-    while(queue):
-        y, x = queue.pop(0)
-
-        for idx in range(4):
-            ny = y + dy[idx]
-            nx = x + dx[idx]
-
-            if(possible(ny, nx)):
-                queue.append((ny, nx))
-                visit[ny][nx] = True
-                temp.append((ny, nx))
-
-    return temp
-
-
-def weight(a, b):
-    result = sys.maxsize
-    for i in a:
-        for j in b:
-            result = min(result, abs(i[0] - j[0]) + abs(i[1] - j[1]) - 1)
-
-    return result
-
-
 n = int(sys.stdin.readline())
+# 미로를 저장하는 list
+miro = []
 
-miro = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
+for i in range(n):
+    l = list(sys.stdin.readline().rstrip())
 
-visit = [[False]*n for _ in range(n)]
-nodes = []
+    miro.append(l)
+
+# visit[i][j][k] -> i는 행 j는 열 k는 i행 j열까지 몇개의 벽을 지나왔는지 최솟값
+visit = [[[False, sys.maxsize] for i in range(n)] for _ in range(n)]
+visit[0][0] = [True, 0]
 
 dy = [1, -1, 0, 0]
 dx = [0, 0, 1, -1]
 
-for i in range(n):
-    for j in range(n):
-        if(miro[i][j] == '1' and not visit[i][j]):
-            nodes.append(bfs(i, j))
-
-for i in range(len(nodes)):
-    if((0, 0) in nodes[i]):
-        start = i
-
-    if((n-1, n-1) in nodes[i]):
-        end = i
-
-my_map = dict()
-
-for i in range(len(nodes)):
-    my_map[str(i)] = dict()
-
-for i in range(len(nodes)//2 + 1):
-    for j in range(i+1, len(nodes)):
-        w = weight(nodes[i], nodes[j])
-        my_map[str(i)][str(j)] = w
-        my_map[str(j)][str(i)] = w
-
+# 우선순위 큐 만들어 줌
 queue = []
-dist = [sys.maxsize]*len(nodes)
-dist[start] = 0
 
-heapq.heappush(queue, [dist[start], start])
+heapq.heappush(queue, [0, 0, 0])
 
+# bfs를 이용해 문제를 풀었음
 while(queue):
-    cur_dist, cur_node = heapq.heappop(queue)
+    k, y, x = heapq.heappop(queue)
 
-    if(dist[cur_node] < cur_dist):
-        continue
+    if(y == n-1 and x == n-1):
+        print(k)
+        break
 
-    for adj, w in my_map[str(cur_node)].items():
-        distance = cur_dist + w
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
 
-        if(distance < dist[int(adj)]):
-            dist[int(adj)] = distance
-            heapq.heappush(queue, [distance, int(adj)])
-
-print(dist[end])
+        if(possible(ny, nx) and visit[y][x][1] < visit[ny][nx][1]):
+            visit[ny][nx][0] = True
+            if(miro[ny][nx] == '0'):
+                visit[ny][nx][1] = k+1
+                heapq.heappush(queue, [k+1, ny, nx])
+            else:
+                visit[ny][nx][1] = k
+                heapq.heappush(queue, [k, ny, nx])
