15a265d4190929527dcb81a2d1485d778275b2cd
yserbane <donggar97@naver.com>
2022-01-23 01:34:18 +0900

    BAEKJOON_1926

--- BAEKJOON/Practice/그래프 탐색/1926.py
+++ BAEKJOON/Practice/그래프 탐색/1926.py
@@ -2,6 +2,8 @@ import sys
 
 input = sys.stdin.readline
 
+# 배열을 벗어나는지 판단
+
 
 def pos(y, x):
     if(0 <= y < n and 0 <= x < m and paper[y][x]):
@@ -13,8 +15,11 @@ n, m = map(int, input().split())
 
 paper = [list(map(int, input().split())) for _ in range(n)]
 
+# 배열을 방문했는지 저장하는 list
 visit = [[False]*m for _ in range(n)]
+# 최대 그림 면적
 result = 0
+# 그림의 수
 result_cnt = 0
 
 dy = [0, 0, -1, 1]
@@ -22,6 +27,7 @@ dx = [-1, 1, 0, 0]
 
 for i in range(n):
     for j in range(m):
+        # 그림이 있고 방문하지 않았다면 bfs를 해준다.
         if(paper[i][j] and not visit[i][j]):
             queue = [(i, j)]
             visit[i][j] = True
