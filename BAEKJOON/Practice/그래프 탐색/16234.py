fc6419ad23ded49d92fa5711f8cef919b2af79f8
yserbane <donggar97@naver.com>
2022-02-05 19:44:48 +0900

    BAEKJOON_16234

--- BAEKJOON/Practice/그래프 탐색/16234.py
+++ BAEKJOON/Practice/그래프 탐색/16234.py
@@ -1,5 +1,9 @@
 import sys
-from collections import deque as dq
+from collections import deque
+
+input = sys.stdin.readline
+
+# 좌표가 배열을 벗어나는지 판단
 
 
 def pos(y, x):
@@ -7,63 +11,64 @@ def pos(y, x):
         return True
     return False
 
+# 해당 좌표에서 연합 국가들을 찾아냄
 
-def bfs(y, x):
-    q = dq([(y, x)])
+
+def make_team(y, x):
+    dq = deque([(y, x)])
+    temp = [(y, x)]
+    total = countries[y][x]
     visit[y][x] = True
-    union = [(y, x)]
+    cnt = 1
 
-    while(q):
-        y, x = q.popleft()
+    while(dq):
+        cy, cx = dq.popleft()
 
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if(pos(ny, nx)):
+                diff = abs(countries[cy][cx] - countries[ny][nx])
 
-            if(pos(ny, nx) and not visit[ny][nx] and l <= abs(arr[y][x] - arr[ny][nx]) <= r):
-                union.append((ny, nx))
-                q.append((ny, nx))
-                visit[ny][nx] = True
+                if(not visit[ny][nx] and l <= diff <= r):
+                    visit[ny][nx] = True
+                    total += countries[ny][nx]
+                    cnt += 1
+                    dq.append((ny, nx))
+                    temp.append((ny, nx))
 
-    if(len(union) == 1):
-        return 0
-
-    num = 0
-
-    for y, x in union:
-        num += arr[y][x]
-
-    num = int(num / len(union))
-
-    for y, x in union:
-        arr[y][x] = num
-
-    return 1
+    return cnt, total, temp
 
 
-n, l, r = map(int, sys.stdin.readline().split())
+n, l, r = map(int, input().split())
 
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-
-dy = [1, -1, 0, 0]
-dx = [0, 0, 1, -1]
-
+countries = [list(map(int, input().split())) for _ in range(n)]
 result = 0
 
+dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+
 while(1):
-    cnt = 0
     visit = [[False]*n for _ in range(n)]
+    move = False
 
-    for i in range(n):
-        for j in range(n):
-            if(not visit[i][j]):
-                cnt += bfs(i, j)
+    for y in range(n):
+        for x in range(n):
+            if(not visit[y][x]):
+                cnt, total, team = make_team(y, x)
+                people = total // cnt
+                # 연합국이 없는경우
+                if(cnt == 1):
+                    continue
 
-    print(arr)
-
-    if(cnt):
-        result += 1
-    else:
+                for ty, tx in team:
+                    countries[ty][tx] = people
+                # 인구이동이 발생한 날인 경우
+                move = True
+    # 인구이동이 일어나지 않았으면 빠져나가준다.
+    if(not move):
         break
+
+    result += 1
 
 print(result)
