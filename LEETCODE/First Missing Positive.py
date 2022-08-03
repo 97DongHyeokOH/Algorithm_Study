5bd6b5d8a4c9c2a79f4b2a61c683e93d247ccf56
DongHyeok <donggar97@naver.com>
2022-08-03 12:32:09 +0900

    폴더 옮김

--- LEETCODE/First Missing Positive.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        num_set = set([i for i in range(1, len(nums)+1)])
-        num_set -= set(nums)
-        
-        if num_set:
-            return min(num_set)
-        return max(nums)+1
