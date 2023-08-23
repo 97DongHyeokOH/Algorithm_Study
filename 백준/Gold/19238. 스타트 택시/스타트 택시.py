538689832bc56bc00b9ae2532ccdcddc9b686597
DongHyeok OH <64296314+97DongHyeokOH@users.noreply.github.com>
2023-08-23 15:54:34 +0900

    [Gold II] Title: 스타트 택시, Time: 244 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.py
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.py
@@ -3,11 +3,13 @@ from collections import deque, defaultdict
 
 input = sys.stdin.readline
 
+# 배열을 벗어나지 않고, 벽을 무시하지 않기 위한 함수
 def pos(y, x):
     if 0 <= y < n and 0 <= x < n and not area[y][x]:
         return True
     return False
 
+# 현재 택시가 가장 가까운 승객이 있는 위치까지 가는 함수
 def go_passenger(y, x):
     dist = [[float('inf')]*n for _ in range(n)]
     
@@ -19,6 +21,7 @@ def go_passenger(y, x):
     while dq:
         d, cy, cx = dq.popleft()
         
+        # 가장 가까운 승객을 찾으면 그 거리와 같은 승객들은 temp에 넣어두고 행, 열이 가장 낮은 승객에게 간다.
         if d <= min_d and passenger_index[(cy,cx)] and not done[passenger_index[(cy,cx)]-1]:
             temp.append((d, cy, cx, passenger_index[(cy,cx)]-1))
             min_d = d
@@ -39,6 +42,7 @@ def go_passenger(y, x):
     else:
         return 0
 
+# 승객을 태우고 도착지까지 가는 함수
 def go_destination(sy, sx, des_y, des_x):
     dist = [[float('inf')]*n for _ in range(n)]
     
@@ -69,9 +73,11 @@ passenger = [list(map(int, input().split())) for _ in range(m)]
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
+# 해당 승객을 태웠는지 안태웠는지 판단하기 위함
 passenger_index = defaultdict(int)
 done = [False]*m
 
+# index에 1을 더해주는 이유는 만약 해당 좌표에 승객이 없으면 defaultdict의 특성상 0을 반환하기 때문에 1을 더해주고, 나중에 index를 통해 승객의 정보를 불러올 때 1을 다시 빼줌
 for i, p in enumerate(passenger):
     passenger_index[(p[0]-1, p[1]-1)] = i+1
 
@@ -80,6 +86,7 @@ cy, cx = start_y-1, start_x-1
 while False in done:
     go_p = go_passenger(cy, cx)
     
+    # 만약 태울 수 있는 승객이 없는 경우
     if not go_p:
         print(-1)
         exit(0)
@@ -88,6 +95,7 @@ while False in done:
     
     oil -= go_p[0] + go_d
 
+    # 승객을 도착지에 내리기 전에 연료가 다 없어지는 경우
     if oil < 0:
         print(-1)
         exit(0)
