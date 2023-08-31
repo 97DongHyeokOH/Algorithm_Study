5aed9e38f7b5668a7c58a957b6b980c41f0dfe32
DongHyeok OH <64296314+97DongHyeokOH@users.noreply.github.com>
2023-08-31 19:27:05 +0900

    [Gold IV] Title: 동전 교환, Time: 72 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/1532. 동전 교환/동전 교환.py
+++ 백준/Gold/1532. 동전 교환/동전 교환.py
@@ -8,14 +8,17 @@ g2, s2, b2 = map(int, input().split())
 
 cnt = 0
 
+# 금화 최소 개수를 맞춰줌
 cnt += max(0, g2 - g1)
 s1 -= max(0, g2 - g1) * 11
 g1 += max(0, g2 - g1)
 
+# 동화 최소 개수를 맞춰줌
 cnt += max(0, math.ceil((b2 - b1) / 9))
 s1 -= max(0, math.ceil((b2 - b1) / 9))
 b1 += max(0, math.ceil((b2 - b1) / 9)) * 9
 
+# 이제 은화 최소 개수를 맞추는데 금화 또는 동화가 부족하면 -1을 출력
 while s1 < s2:
     cnt += 1
     
