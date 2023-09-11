c2eafa6e9dc0445be97350e2c7aa3c2a8a6b2e31
DongHyeok OH <64296314+97DongHyeokOH@users.noreply.github.com>
2023-09-11 20:08:07 +0900

    [Gold IV] Title: 음식 평론가, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1188. 음식 평론가/음식 평론가.py
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.py
@@ -6,11 +6,8 @@ n, m = map(int, input().split())
 
 ans = 0
 
-while n and m:
-    if n == m:
-        break
-    
-    if n > m:
+while n:
+    if n >= m:
         n -= m
     else:
         m -= n
