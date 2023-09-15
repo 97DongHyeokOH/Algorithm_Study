146cfebd894eedaaa3857ae9f487a8bacdea2624
DongHyeok OH <64296314+97DongHyeokOH@users.noreply.github.com>
2023-09-15 11:02:21 +0900

    [Gold IV] Title: 운동, Time: 1104 ms, Memory: 122644 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -16,10 +16,10 @@ for _ in range(e):
     cost[s][e] = c
 
 # 플로이드-워셜로 모든 노드간 최단거리를 구함(i번 노드 -> i번 노드 값 중에서 최소값을 찾음)
-for i in range(1, v+1):
-    for j in range(1, v+1):
-        for k in range(1, v+1):
-            cost[i][k] = min(cost[i][k], cost[i][j] + cost[j][k])
+for k in range(1, v+1):
+    for i in range(1, v+1):
+        for j in range(1, v+1):
+            cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j])
 
 for i in range(1, v+1):
     ans = min(ans, cost[i][i])
